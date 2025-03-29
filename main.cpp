f08ace920aae09d138d95eadbf032a29e73a9528
michin <niesmialymichal@gmail.com>
2025-03-29 10:16:48 +0100

    init

---
+++ main.cpp
@@ -0,0 +1,16 @@
+/**
+ * @file main.cpp
+ * @author your name (micha.brodziakw@gmail.com)
+ * @brief Main file is provided in this project only for testing purposes. It 
+ * is however not needed to use of library.
+ * !!! TO USE LIBRARY FOLLOW README.md !!!
+ * @version 0.1
+ * @date 2025-03-29
+ */
+
+ #include "fibered/Fibered.hpp:
+
+ int main()
+ {
+
+ }
